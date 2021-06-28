519c8296146895a59c567999cbb6f3ff76433335
An Tr <andrecee.tnn@gmail.com>
2021-06-28 21:28:30 +0700

    variantsValue is not iterable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  mode: "jit",
+  mode: "jit", 
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {},
   variants: {
     extend: {
-      animation: ["group-hover"],
+      animation: ["group-hover"], 
       screens: {
         "3xl": "1600px",
       },
