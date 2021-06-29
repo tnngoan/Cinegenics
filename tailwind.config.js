f6b10bfe9ecf977578d5f31f67acfa03c32c32a1
An Tr <andrecee.tnn@gmail.com>
2021-06-29 18:46:51 +0700

    removed screens extension, anition still not working

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-  mode: "jit", 
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",  // this could be the err however I tried removing it and it didn't change a thing
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],  //this should be rm together with jit
   darkMode: false, // or 'media' or 'class'
   theme: {},
   variants: {
     extend: {
       animation: ["group-hover"], 
-      screens: {
-        "3xl": "1600px",
-      },
     },
   },
   plugins: [require("tailwind-scrollbar-hide")],
